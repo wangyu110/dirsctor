98fb0c97e1f6c40cbc7961f0ee6d0ad882525a21
wangyu110 <1225974971@qq.com>
2016-12-21 11:32:53 +0800

    dirsctor

---
+++ dirsctor.sql
@@ -0,0 +1,17 @@
+create table dirsctor(
+id int auto_increment primary key,
+name varchar(128),
+biography varchar(512),
+film_maiden_work varchar(128),
+Film_word varchar(128),
+Film_award varchar(128)
+);
+insert into dirsctor values (null,'钢铁侠3','出生于1961年美国宾夕法尼亚州，匹兹堡','黑色喜剧《小贼·美女·妙探》','黑色喜剧《小贼·美女·妙探》','圣地亚哥影评人协会奖');
+
+insert into dirsctor values (null,'寒战2','出生于1967年中国香港','《阳光地狱之人肉市场》','？？','第32届香港电影金像奖最佳编剧奖');
+
+insert into dirsctor values (null,'钢锯岭','出生于1956年美国纽约州皮克斯基尔',' 《夏日城市》','??','金球奖最佳导演奖');
+
+insert into dirsctor values (null,'我不是潘金莲','1958年出生于北京','《遭遇激情》','《编辑部的故事》','台湾电影金马奖  最佳导演 (获奖)');
+
+insert into dirsctor values (null,'那些年','本名柯景腾,1978年出生于台湾彰化','??','??','第50届 十大杰出青年');
